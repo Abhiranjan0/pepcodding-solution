26de897c5d597f6d946a51a71fcfe62c264e239b
Abhiranjan0 <111702473+Abhiranjan0@users.noreply.github.com>
2022-09-23 14:57:01 +0530

    Create Rotate an array.java

--- Function and array/Rotate an array.java
+++ Function and array/Rotate an array.java
@@ -25,6 +25,11 @@ public class Main{
   }
 
   public static void rotate(int[] a, int k){
+    k=k%a.length;
+    if(k<0)
+    {
+      k=k+a.length;
+    }
     reverse(a,0,a.length-k-1);
     reverse(a,a.length-k,a.length-1);
     reverse(a,0,a.length-1);
